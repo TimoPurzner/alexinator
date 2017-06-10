523edb946acfb3656f7d29aaea6c9116f063c58a
ZeroSoulEater <t1@purzner.de>
2017-06-10 02:23:30 +0200

    Add help
    provide help depents on the current state

--- intents/help.js
+++ intents/help.js
@@ -1,14 +1,41 @@
+/*####################################
+  Intent: help
+  Slots:
+  Gives the player some help with whitch words he can use now
+######################################*/
 module.exports = class TestIntent {
   constructor() {
-    this.name = "help";
+    this.name = "AMAZON.HelpIntent";
     this.utterances = ["Hilfe"],
                       ["Was soll ich tun?"],
                       ["Was kann ich tun?"],
                       ["Was ist das hier?"]
   }
   execute(req, rsp) {
-    console.log("You asked for help")
-    rsp.say("Alexinator ist ein Spiel das versucht eine Person zu erraten, an die du denkst! Dazu stelle ich dir "+
-            "einige Fragen. Beantworte diese einfach mit Ja, Nein, ich weiß nicht, Wahrscheinlich, Wahrscheinlich nicht");
+
+    res.shouldEndSession(false);
+    //Get current question
+    var session = req.getSession();
+    var status = session.get('status');
+
+    switch (status) {
+      case 'launch':
+          rsp.say("Du hast noch kein Spiel angefangen, wenn du ein Spiel beginnen möchtest sag einfach starte eine Spiel").reprompt("Hilfe");
+        break;
+      case 'question':
+          rsp.say("Die Aktuelle Frage ist:" + session.get("akinatorQuestion") + "Antworte mit Ja, Nein ,ich weiß nicht, wahrscheinlich oder wahrscheinlich nicht").reprompt("Hilfe");
+      break;
+      case 'finished':
+          rsp.say("Ich habe die Letze Person erraten an die du gedacht hast. Sage Starte eine Runde um ein neues Spiel zu begeinnen").reprompt("Hilfe");
+      break;
+      case 'win':
+        resp.say("Ich wollte wissen ob du an " + session.get("akinatorName") + " gedacht hast".reprompt("Hilfe");
+        break;
+      default:
+        rsp.say("Alexinator ist ein Spiel das versucht eine Person zu erraten, an die du denkst! Dazu stelle ich dir "+
+              "einige Fragen. Beantworte diese einfach mit Ja, Nein, ich weiß nicht, Wahrscheinlich, Wahrscheinlich nicht");
+    }
+
+
   }
 }
